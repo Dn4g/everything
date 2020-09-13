536eae1cb43a4f5e23c22281ba56509e412e92fd
Dn4g <dn4g@yandex.ru>
2020-09-14 01:56:24 +0800

    Update move.sh

--- move.sh
+++ move.sh
@@ -3,6 +3,7 @@
 ssh-keyscan -H 62.109.17.38 >> ~/.ssh/known_hosts
 #or just download shared key
 ssh-keygen -q -f id_banana -N ''
+
 #cat /root/support/banaclone/exclude.txt
 /dev/
 /proc/
@@ -16,7 +17,9 @@ ssh-keygen -q -f id_banana -N ''
 /lib/modules/
 /usr/local/mgr5/var/.xmlcache
 /var/cache/yum
+
 rsync -avP / --exclude-from /root/support/banaclone/exclude.txt -e 'ssh -i .ssh/id_banana' root@62.109.17.38:/mnt/
+
 rsync -avP /var/lib/mysql/ -e 'ssh -i .ssh/id_banana' root@62.109.17.38:/mnt/var/lib/mysql/
 service mysqld stop
 rsync -avP /var/lib/mysql/ -e 'ssh -i .ssh/id_banana' root@62.109.17.38:/mnt/var/lib/mysql/
@@ -38,6 +41,7 @@ grub-install /dev/vda
 grep -Rils 62.109.8.10 /etc/ | xargs sed -i s'|62.109.8.10|62.109.17.38|g'
 grep -Rils 62.109.8.10 /var/named | xargs sed -i s'|62.109.8.10|62.109.17.38|g'
 grep -Rils 62.109.8.10 /usr/local/mgr5/etc/ihttpd.conf | xargs sed -i s'|62.109.8.10|62.109.17.38|g'
-sqlite3
+# чтоб не забыть, так, конечно, не работает))
+sqlite3 /usr/local/mgr5/etc/ispmgr.db  -e 'update webdomain_ipaddr set value='62.109.24.79;
 reboot
 
