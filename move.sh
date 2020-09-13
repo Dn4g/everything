6ebe6abba990bdff19a7b06e41f8c9b86d38f457
Dn4g <dn4g@yandex.ru>
2020-09-14 01:46:25 +0800

    Add files via upload

---
+++ move.sh
@@ -0,0 +1,43 @@
+
+#source
+ssh-keyscan -H 62.109.17.38 >> ~/.ssh/known_hosts
+#or just download shared key
+ssh-keygen -q -f id_banana -N ''
+#cat /root/support/banaclone/exclude.txt
+/dev/
+/proc/
+/sys/
+/boot/
+/var/lib/docker/
+/var/lib/mysql/
+/etc/sysconfig/network-scripts/
+/var/www/*/data/tmp/
+/var/www/*/data/mod-tmp/
+/lib/modules/
+/usr/local/mgr5/var/.xmlcache
+/var/cache/yum
+rsync -avP / --exclude-from /root/support/banaclone/exclude.txt -e 'ssh -i .ssh/id_banana' root@62.109.17.38:/mnt/
+rsync -avP /var/lib/mysql/ -e 'ssh -i .ssh/id_banana' root@62.109.17.38:/mnt/var/lib/mysql/
+service mysqld stop
+rsync -avP /var/lib/mysql/ -e 'ssh -i .ssh/id_banana' root@62.109.17.38:/mnt/var/lib/mysql/
+service mysql start
+rsync -avP /var/lib/docker/ root@62.109.17.38:/mnt/var/lib/
+
+#final
+chroot /mnt/ /bash
+mkdir -p /root/support/banaclone/
+rsync -avP /etc /root/support/banaclone/
+rsync -avP /root --exclude=/root/support/ /root/support/banaclone/
+#after resync
+yum clean all
+yum install kernel
+yum reinstall dbus
+dracut -f
+rm /boot/grub/grub.cfg
+grub-install /dev/vda
+grep -Rils 62.109.8.10 /etc/ | xargs sed -i s'|62.109.8.10|62.109.17.38|g'
+grep -Rils 62.109.8.10 /var/named | xargs sed -i s'|62.109.8.10|62.109.17.38|g'
+grep -Rils 62.109.8.10 /usr/local/mgr5/etc/ihttpd.conf | xargs sed -i s'|62.109.8.10|62.109.17.38|g'
+sqlite3
+reboot
+
